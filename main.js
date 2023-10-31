3a45f3050ca2cf6473e6dfcde95183951d5d7bba
Alfredo Ramirez <darkfalz674@gmail.com>
2023-10-31 11:14:43 -0400

    .

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
 
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
 
 app.whenReady().then(() => {
-    createWindow()
+  createWindow()
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-
-    app.on('window-all-closed', () => {
-        if (process.platform !== 'darwin') app.quit()
-    })
-
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
 })
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
-
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600
-    })
-
-    win.loadFile('index.html')
-}
